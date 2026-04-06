c43d615bfc65a093b7dbd6c6c08a222e1ea048be
Jõao Luiz Candaten Fonseca <joaoluizcfonseca@gmail.com>
2026-04-06 20:55:45 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/exercicio_21/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        int num = 34;
+        while (num <= 57) {
+            System.out.println(num);
+            num += 2;
+
+        }
+    }
+}
